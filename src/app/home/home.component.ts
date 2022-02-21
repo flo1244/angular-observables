630e2f57461405482c136c28245b4cd08347775e
flo_h <flo.1244@gmail.com>
2022-02-20 19:13:12 -0500

    Added custom observable, subjects, operators

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,8 +7,10 @@
 
 
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription,  Observable } from 'rxjs';
+//import Observable to use with custom observable. 
 
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -23,9 +25,42 @@ export class HomeComponent implements OnInit, OnDestroy {
   //building our own observables. we are import interval wil fire new value every second. This observable is going to keep emitting this event until we unsubscribe to prevent memory leaks. 
   //Notice the count continues even when we go back to home but starts a new observable as well. 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+
+    //Custom observable
+    //observer will listen  to the argument
+    const customIntervalObservable =  Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count); //can call next to emit new value.
+        if (count === 2) {
+          observer.complete(); //will complete our observable.
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      }, 1000);
     });
+
+    //operators with pipe method and map method. Mapping over data. 
+    // customIntervalObservable.pipe(map((data: number) => {
+    //   return 'Round: ' +  (data + 1);
+    // }));
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+   }), map((data: number) => {
+      return 'Round: ' +  (data + 1);
+    })).subscribe(data => {
+      console.log(data);
+   }, error => {
+     console.log(error);
+     alert(error.message);//will throw an error and observable will stop
+   }, () => {
+     console.log('Completed!');
+    })
   }
 
   //now we unsubscribe. we clear that subscription  prevent memory leaks because we are not keeping old subscriptions. 
